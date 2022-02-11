45ea57e4c153357436a4a6d39c926272e1b945e7
Benoit Blanchon <git@benoitblanchon.fr>
2022-02-11 14:44:43 +0100

    Convert from Bootstrap 5 to Bootstrap 4

--- movie_collection/static/dialog.js
+++ movie_collection/static/dialog.js
@@ -1,23 +1,21 @@
 ;(function () {
-  const modal = new bootstrap.Modal(document.getElementById("modal"))
-
-  htmx.on("htmx:afterSwap", function (e) {
+  htmx.on("htmx:afterSwap", (e) => {
     // Response targeting #dialog => show the modal
     if (e.detail.target.id == "dialog") {
-      modal.show()
+      $(e.detail.target).modal("show")
     }
   })
 
   htmx.on("htmx:beforeSwap", (e) => {
     // Empty response targeting #dialog => hide the modal
     if (e.detail.target.id == "dialog" && !e.detail.xhr.response) {
-      modal.hide()
+      $(e.detail.target).modal("hide")
       e.detail.shouldSwap = false
     }
   })
 
   // Remove dialog content after hiding
   htmx.on("hidden.bs.modal", () => {
-    document.getElementById("dialog").innerHTML = ""
+    $("#modal").empty()
   })
 })()
