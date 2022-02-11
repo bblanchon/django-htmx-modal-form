a72211c6b1c7b6ad784f31854a4e91368074732d
Benoit Blanchon <git@benoitblanchon.fr>
2022-02-11 14:45:10 +0100

    Use arrow function

--- movie_collection/static/dialog.js
+++ movie_collection/static/dialog.js
@@ -1,7 +1,7 @@
 ;(function () {
   const modal = new bootstrap.Modal(document.getElementById("modal"))
 
-  htmx.on("htmx:afterSwap", function (e) {
+  htmx.on("htmx:afterSwap", (e) => {
     // Response targeting #dialog => show the modal
     if (e.detail.target.id == "dialog") {
       modal.show()
